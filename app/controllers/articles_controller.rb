ab9b94ee39e703c2d5de82a363bba974012c7c4f
beket <lbeketl@mail.com>
2021-02-07 00:14:58 +0200

    Finishing!

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,56 @@
+class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @articles = Article.all
+    end
+
+    def show
+        set_article
+    end
+    
+    def new 
+        @article = Article.new
+    end
+
+    def edit
+        set_article
+    
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = 'Article was updated successfuly'
+            redirect_to @article
+        else render 'edit'
+        end
+    end
+
+    def destroy
+        set_article
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    def create
+        @article = Article.new(article_params)
+       if  @article.save
+        flash[:notice] = 'Article was created successfuly.'
+        redirect_to @article
+       else
+        render 'new'
+       end
+    end
+
+    private
+
+        def set_article
+            @article = Article.find(params[:id])
+        end
+
+        def article_params
+            params.require(:article).permit(:title, :description)
+        end
+
+end
